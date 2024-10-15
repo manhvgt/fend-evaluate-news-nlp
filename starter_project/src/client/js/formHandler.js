786f07d64afbd344d096a675b4364a44a82bd33f
Manh Vuong <vgtuanmanh@outlook.com>
2024-10-16 01:44:39 +0800

    completed main functions
    - call and handle api response
    - service worker

--- starter_project/src/client/js/formHandler.js
+++ starter_project/src/client/js/formHandler.js
@@ -1,30 +1,110 @@
 // Replace checkForName with a function that checks the URL
-import { checkForName } from './nameChecker'
+// import { checkForName } from './nameChecker'
 
 // If working on Udacity workspace, update this with the Server API URL e.g. `https://wfkdhyvtzx.prod.udacity-student-workspaces.com/api`
 // const serverURL = 'https://wfkdhyvtzx.prod.udacity-student-workspaces.com/api'
-const serverURL = 'https://localhost:8000/api'
+const serverURL = 'http://localhost:8000/api'
 
+
+// Variable
+const submitButton = document.getElementById('submitButton');
 const form = document.getElementById('urlForm');
+const outModel = document.getElementById('outModel');
+const outSubjectivity = document.getElementById('outSubjectivity');
+const outConcept = document.getElementById('outConcept');
+const outDetails = document.getElementById('outDetails');
+
+// Add listener
 form.addEventListener('submit', handleSubmit);
 
-function handleSubmit(event) {
+// Export the handleSubmit function
+export async function handleSubmit(event) {
     event.preventDefault();
 
     // Get the URL from the input field
     const formText = document.getElementById('name').value;
 
-    // This is an example code that checks the submitted name. You may remove it from your code
-    checkForName(formText);
-    
     // Check if the URL is valid
- 
-        // If the URL is valid, send it to the server using the serverURL constant above
-      
+    if(!validateUrl(formText)) {
+        console.error('Invalid url: ', formText);
+        alert("Please enter a valid url then try again");
+        return;
+    }
+    
+    // If the URL is valid, send it to the server using the serverURL constant above
+    enableSubmitButton(false);
+    await sendToServer(formText);
+    enableSubmitButton(true);
 }
 
 // Function to send data to the server
+export async function sendToServer(url) {
+    if(!url || url.trim() === "") {
+        console.error("The text variable is null or empty.");
+        return;
+    }
 
-// Export the handleSubmit function
-export { handleSubmit };
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({ 'url': url})
+    };
+    // console.log('requestOptions', requestOptions);
 
+    // disable submit button
+
+
+    try {
+        // Send rquest
+        const response = await fetch(serverURL, requestOptions);
+        // console.log(response);
+
+        // check response
+        if(!response.ok) {
+            // throw new Error(`Ressponse status: ${response.statusText}`);
+        }
+
+        // response ok. update view
+        updateView(await response.json());
+
+    } catch(e) {
+        console.error('Failed to process the request.! ', e);
+        alert('Failed to process the request!', e);
+    }
+}
+
+
+// Function to validate input form value
+export function validateUrl(url) {
+    try {
+        new URL(url);
+        return true;
+    }
+    catch(e) {
+        return false;
+        
+    }
+}
+
+// Function to upldate View
+export function updateView(data) {
+    console.log(data);
+    outModel.innerHTML = `Model: ${data.model}`;
+    outSubjectivity.innerHTML = `Subjectivity: ${data.subjectivity}`;
+    outAgreement.innerHTML = `Agreement: ${data.agreement}`;
+    // Use more details data for future function...
+    // outConcept.innerHTML = `Concept: ${data.concept}`;
+    // outDetails.innerHTML = `Details response: ${JSON.stringify(data)}`;
+}
+
+export function enableSubmitButton(enable) {
+    if(enable) {
+        submitButton.disabled = false;
+        submitButton.classList.remove('disabled');
+        submitButton.textContent = 'Submit';
+    } else {
+        submitButton.disabled = true;
+        submitButton.classList.add('disabled');
+        submitButton.textContent = 'Processing...';
+    }
+}
--- starter_project/src/server/index.js
+++ starter_project/src/server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(`Your API key is ${process.env.API_KEY}`);
+// const meaningCloud = require('meaning-cloud');
 
 const app = express();
 
@@ -16,21 +16,52 @@ app.use(express.static('dist'));
 console.log(__dirname);
 
 // Variables for url and api key
+const API_URL = process.env.API_ID;
+const API_KEY = process.env.API_KEY;
 
-
+// GET Route
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html');
+    res.sendFile('dist/index.html');
     res.send("This is the server API page, you may access its services via the client app.");
 });
 
 
 // POST Route
+app.post('/api', async (req, res) => {
+    // setup form data
+    const { url } = req.body;
+    const formdata = new FormData();
+    formdata.append("key", API_KEY);
+    // formdata.append("of", "json"); // Default is json
+    // formdata.append("lang", "auto");  // 2-letter code, like en es fr ...
+    formdata.append("lang", "auto");
+    formdata.append("url", url);
+    
+    const requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+      };     
 
+      console.log('requestOptions', requestOptions);
+
+    // try to fetch data
+    try {
+        const response = await fetch(API_URL, requestOptions);
+        if(!response.ok) {
+          console.error('Failed to call API, error', response.status);
+        }
+        const data = await response.json();
+        res.json(data); 
+        // console.log(res);
+    }
+    catch(e) {
+      console.error('error', e);
+    }
+});
 
 
 // Designates what port the app will listen to for incoming requests
 app.listen(8000, function () {
     console.log('Example app listening on port 8000!');
 });
-
-
